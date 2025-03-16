d95ab965911f415ad089b0df662488cf2c46814b
Equo <david.derny@ik.me>
2025-03-16 16:37:07 +0100

    fix: Improve memory handling in vec_extend function to prevent leaks

--- src/vec/vec_extend.c
+++ src/vec/vec_extend.c
@@ -6,11 +6,12 @@
 /*   By: dderny <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 16:49:59 by dderny            #+#    #+#             */
-/*   Updated: 2025/03/15 18:53:24 by dderny           ###   ########.fr       */
+/*   Updated: 2025/03/16 16:36:59 by dderny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_vector.h"
+#include "libft.h"
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -20,9 +21,14 @@ ssize_t	vec_extend(t_vector *vec, ssize_t x)
 
 	vec->size = vec->size << x;
 	data = malloc(vec->size * vec->type_size);
-	free(vec->data);
 	if (!data)
+	{
+		free(vec->data);
 		return (0);
+	}
+	ft_memcpy(data, vec->data, vec->actual_size);
+	ft_bzero(data + vec->actual_size, vec->size - vec->actual_size);
+	free(vec->data);
 	vec->data = data;
 	return (vec->size);
 }
