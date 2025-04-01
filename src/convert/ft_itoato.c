34771dcdd3b7141b21a2c8c283dcc14c1a1a9171
dderny <david.derny@ik.me>
2025-04-01 21:10:50 +0200

    fix itoato

--- src/convert/ft_itoato.c
+++ src/convert/ft_itoato.c
@@ -6,12 +6,13 @@
 /*   By: dderny <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 17:39:23 by dderny            #+#    #+#             */
-/*   Updated: 2025/04/01 15:01:50 by dderny           ###   ########.fr       */
+/*   Updated: 2025/04/01 21:04:02 by dderny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_itoato(int n, char *dst)
 {
+	int		rlen;
 	int		len;
 	long	nb;
 
@@ -23,6 +24,7 @@ int	ft_itoato(int n, char *dst)
 	nb = n;
 	while (nb && ++len)
 		nb /= 10;
+	rlen = len;
 	nb = (long)n;
 	if (nb < 0)
 	{
@@ -34,5 +36,5 @@ int	ft_itoato(int n, char *dst)
 		dst[len] = nb % 10 + '0';
 		nb /= 10;
 	}
-	return (len);
+	return (rlen);
 }
