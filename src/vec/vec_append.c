6dd4b24f2e29396bccfd957f4c72c236fe0c90a3
dderny <dderny@student.42lyon.fr>
2025-04-15 01:35:38 +0200

    fix: correct return value in vec_append function and update file headers

--- src/vec/vec_append.c
+++ src/vec/vec_append.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vec_append.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dderny <dderny@42lyon.fr>                  +#+  +:+       +#+        */
+/*   By: dderny <dderny@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 16:29:56 by dderny            #+#    #+#             */
-/*   Updated: 2025/04/02 20:51:14 by dderny           ###   ########.fr       */
+/*   Updated: 2025/04/15 01:35:20 by dderny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	vec_append(t_vector *vec, void *val)
 {
-	if (vec->actual_size >= vec->size && vec_extend(vec, 1))
-		return (0);
+	if (vec->actual_size >= vec->size && !vec_extend(vec, 1))
+		return (1);
 	return (vec_set(vec, vec->actual_size, val));
 }
