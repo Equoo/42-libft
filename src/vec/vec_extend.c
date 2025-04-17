a1b9698402d5dcfb6112eb0bbf0270f8231315af
dderny <david.derny@ik.me>
2025-04-17 22:05:18 +0200

    fix: update author email in vec_extend.c and adjust formatting

--- src/vec/vec_extend.c
+++ src/vec/vec_extend.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vec_extend.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dderny <dderny@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: dderny <dderny@42lyon.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 16:49:59 by dderny            #+#    #+#             */
-/*   Updated: 2025/04/15 01:34:12 by dderny           ###   ########.fr       */
+/*   Updated: 2025/04/17 22:04:46 by dderny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ ssize_t	vec_extend(t_vector *vec, ssize_t x)
 	}
 	ft_memcpy(data, vec->data, vec->actual_size * vec->type_size);
 	ft_bzero(data + vec->actual_size * vec->type_size,
-			(vec->size - vec->actual_size) * vec->type_size);
+		(vec->size - vec->actual_size) * vec->type_size);
 	free(vec->data);
 	vec->data = data;
 	return (vec->size);
