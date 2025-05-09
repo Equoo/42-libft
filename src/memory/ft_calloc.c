31fcc62a65add85b13312a3ce1bc6166b3ce45fa
dderny <david.derny@ik.me>
2025-05-09 07:12:33 +0200

    add calloc

---
+++ src/memory/ft_calloc.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dderny <dderny@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/09 14:48:23 by dderny            #+#    #+#             */
+/*   Updated: 2025/05/09 01:32:32 by dderny           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*array;
+
+	array = NULL;
+	if ((unsigned long long)nmemb * (unsigned long long)size > SIZE_MAX)
+		return (NULL);
+	array = malloc(nmemb * size);
+	if (!array)
+		return (NULL);
+	ft_memset(array, 0, nmemb * size);
+	return (array);
+}
